be206bb359683b8fa3b564233b9c880ca54944e8
Anand-1516 <banandakrishnan6@gmail.com>
2025-09-23 14:57:02 +0000

    new commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello my world, this is my first task, CI/CD Pipeline is working automatically using EC2! 🚀");
+  res.send("Hello my world, CI/CD Pipeline is working automatically using EC2! 🚀");
 });
 
 // Bind to 0.0.0.0 so it works inside Docker/EC2
